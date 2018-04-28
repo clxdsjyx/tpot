4d49ddb02a83ab7da79d13ed481b01a5cfe7e4da
崔凌逍 <clxdsjyx@163.com>
2018-04-28 21:35:19 +0800

    test

--- tpot/_version.py
+++ tpot/_version.py
@@ -21,8 +21,6 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public
 License along with TPOT. If not, see <http://www.gnu.org/licenses/>.
 
-
-my mode
 """
 
 __version__ = '0.9.3'
